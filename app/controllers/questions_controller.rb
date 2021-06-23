c4feb9b5c88ba82fa36bb6111d2c3ff22e1788e5
thomas puchaux <tomapuch@gmail.com>
2021-06-23 17:45:30 +0200

    pundit questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsController < ApplicationController
+    
     def show
         @question = Question.find(params[:id])
+        authorize @question
     end
+
+
+    # def question_params
+    #     params.require(:questions).permit(:content, :category, :order)
+    # end
 end
