3d4167bc8cac6df2f5deea26babfc71182ded48f
thomas puchaux <tomapuch@gmail.com>
2021-06-25 14:53:56 +0200

    modal et progressbar

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,16 @@
 class QuestionsController < ApplicationController
     # skip_after_action :verify_policy_scoped
     before_action :skip_authorization, only: :save_to_session
+    skip_before_action :authenticate_user!
+
     
     def index
         @questions = Question.all
     end
 
     def save_to_session
-        session[:question_state] = {counter: 2}
+        number = session[:question_state]["counter"].to_i + 1
+        session[:question_state] = {counter: number}
         session[:test] = "hello"
     end
 end
--- app/javascript/plugins/quiz.js
+++ app/javascript/plugins/quiz.js
@@ -3,27 +3,44 @@ import { fetchWithToken } from "../utils/fetch_with_token";
 const answerCounter = () => {
   const btnNo = document.querySelectorAll(".btn-no");
   const btnYes = document.querySelectorAll(".btn-yes");
+  const progressBar = document.querySelector(".progress-bar")
+  // const modal = document.getElementById("my-modal");
+  
+  
+
 
   let counter = 0;
+  let count = 0;
   
   btnNo.forEach((btn) => {
     btn.addEventListener("click", event => {
+      count ++;
+      progress();
       counter--; 
       postData();
-      
+      if(count == 6) {
+        $('#myModal').modal('show');
+        // modal.style.display = "block";
+      }   
     });
   })
 
   btnYes.forEach((btn) => {
     btn.addEventListener("click", event => {
+      count ++;
+      progress();
       counter++;
       postData();
+      if(count == 6) {
+        $('#myModal').modal('show');
+        // modal.style.display = "block";
+      }
     });
   })
 
   const postData = () => {
     const question_data = {counter: counter};
-
+  
     fetchWithToken('/save_to_session', {
       method: "POST",
       // redirect: "follow",
@@ -39,6 +56,12 @@ const answerCounter = () => {
           // console.log(data)
       })
   };
+
+ const progress = () => {
+   let width = parseInt(progressBar.style.width, 10);
+   width += 20;
+   progressBar.style.width = `${width}%`
+ };
 }
 
 export { answerCounter }
