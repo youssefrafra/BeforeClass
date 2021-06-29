f72162a56662e225b4d9c725cbf74ec9815c4957
thomas puchaux <tomapuch@gmail.com>
2021-06-29 16:30:05 +0200

    cookies bientôt terminés

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,12 @@
 class QuestionsController < ApplicationController
     # skip_after_action :verify_policy_scoped
-    before_action :skip_authorization, only: :save_to_session
+    before_action :skip_authorization, only: :save
     skip_before_action :authenticate_user!
 
     
     def index
-        if session[:question_state]["done"]
-            redirect_to dashboard_path
+        if session[:question_state] && session[:question_state]["done"] 
+            redirect_to new_user_registration_path
         else	
         @questions = Question.all
         session[:question_state] = {counter: 0, done: false}
