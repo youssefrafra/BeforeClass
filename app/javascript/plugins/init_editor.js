793facc8ac21cef15cb8d10e2e974864c61a7d42
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-06-29 13:54:27 +0200

    jeux avec editeur à finir

--- app/javascript/plugins/init_editor.js
+++ app/javascript/plugins/init_editor.js
@@ -23,17 +23,34 @@ const intEditor = () => {
             mode:"javascript"
         });
         const runBtn = document.getElementById('run-btn');
+        const resetBtn = document.getElementById('reset-btn');
         let previewWindow = document.getElementById('preview-window').contentWindow.document;
-        let codehtml = '<html><head><title></title></head><body><h1>Hello world.</h1></body></html>' 
+        let codehtml = `<html>
+        <head><title></title></head>
+        <body>
+        <h1>Hello world.</h1>
+        <img src="https://source.unsplash.com/random/300x300">
+        </body>
+        </html>`;
+        // let codecss = `<style></style>`
+
         previewWindow.open();
         previewWindow.write(codehtml);
         previewWindow.close();
         runBtn.addEventListener('click', ()=> {
-            let htmlCode = htmlEditor.getValue();
-            let cssCode = "<style>" + cssEditor.getValue() + "</style>";
+            // let htmlCode = htmlEditor.getValue();
+            // let cssCode = "<style>" + cssEditor.getValue() + "</style>";
             let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
             previewWindow.write(codehtml+jsCode);
             previewWindow.close();
+        })
+        resetBtn.addEventListener('click', ()=> {
+            // let jsCode = "<scri" + "pt>" + "</scri" + "pt>";
+            // document.getElementById('preview-window').contentDocument.location.reload(true);
+            // previewWindow.open();
+            // previewWindow.write(codehtml);
+            // previewWindow.close();
+            document.location.reload(true);
         })
     }
 }
