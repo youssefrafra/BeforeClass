50bba48319fc45c324805bfcdb09c7e2c85724d5
thomas puchaux <tomapuch@gmail.com>
2021-06-30 18:05:25 +0200

    src + boolean modal

--- app/javascript/plugins/devschool_modal.js
+++ app/javascript/plugins/devschool_modal.js
@@ -2,6 +2,9 @@ const devSchoolModal = () => {
 
 const cardSchool = document.querySelectorAll('.card-school');
 const modalSchool = document.getElementById('show-school');
+
+const logo = modalSchool.querySelector(".school-logo")
+
 const name = modalSchool.querySelector(".school-title");
 const address = modalSchool.querySelector(".school-address");
 const description = modalSchool.querySelector(".school-description");
@@ -13,22 +16,31 @@ const price = modalSchool.querySelector(".school-price");
 const category = modalSchool.querySelector(".school-category");
 const url = modalSchool.querySelector(".school-url");
 
-// const logo = modalSchool.querySelector(".school-logo");
+    
 
-
-
-  cardSchool.forEach((school) => {
+    cardSchool.forEach((school) => {
     school.addEventListener("click", (event) => {
       name.innerText = `${event.currentTarget.dataset.name}`;
       address.innerText = `${event.currentTarget.dataset.address}`;
       description.innerText = `${event.currentTarget.dataset.description}`;
       duration.innerText = `${event.currentTarget.dataset.duration}`;
-      payment.innerText = `${event.currentTarget.dataset.payment}`;
-      certifying.innerText = `${event.currentTarget.dataset.certifying}`;
-      remote.innerText = `${event.currentTarget.dataset.remote}`;
+      payment.innerText = `${event.currentTarget.dataset.payment === "true" ? "Oui" : "Non"}`; 
+      certifying.innerText = `${event.currentTarget.dataset.certifying === "true" ? "Oui" : "Non"}`;
+      remote.innerText = `${event.currentTarget.dataset.remote === "true" ? "Oui" : "Non"}`;
       price.innerText = `${event.currentTarget.dataset.price}`;
       category.innerText = `${event.currentTarget.dataset.category}`;
       url.innerText = `${event.currentTarget.dataset.url}`;
+
+      logo.src = `${event.currentTarget.dataset.logo}`;
+
+
+
+      
+
+
+
+
+
       $('#show-school').modal('show');
     });
   })
